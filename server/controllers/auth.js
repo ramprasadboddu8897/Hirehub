d91301db97e80b0b1f71557f126cbe1ec646e316
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2025-07-02 09:04:18 +0530

    Update auth.js
    changed the import User from '../models/user.js';

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,4 @@
-import User from '../models/User.js';
+import User from '../models/user.js';
 import UserProfile from '../models/UserProfile.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
